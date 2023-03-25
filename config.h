6a391a90cf436c0b949f4bb6340877341e0ff905
Lora <lorabrekke110@gmail.com>
2023-03-25 17:43:21 -0600

    Modified the colour

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#282a36";
-static const char col_purple[]      = "#bd93f9";
+static const char col_purple[]      = "#bc3434";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
